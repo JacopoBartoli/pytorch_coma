fcf88682abd0e8ddcad696f55b28285ed856cf90
jacopo <bartolijacopo@gmail.com>
2021-02-12 22:37:37 +0100

    Some changes.

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ class Coma(torch.nn.Module):
         self.cheb = torch.nn.ModuleList([ChebConv_Coma(self.filters[i], self.filters[i+1], self.K[i])
                                          for i in range(len(self.filters)-2)])
         self.cheb_dec = torch.nn.ModuleList([ChebConv_Coma(self.filters[-i-1], self.filters[-i-2], self.K[i])
-                                             for i in range(len(self.filters)-2)])
+                                             for i in range(len(self.filters)-1)])
         self.cheb_dec[-1].bias = None  # No bias for last convolution layer
         self.pool = Pool()
         self.enc_lin = torch.nn.Linear(self.downsample_matrices[-1].shape[0]*self.filters[-1], self.z)
@@ -69,10 +69,12 @@ class Coma(torch.nn.Module):
         x = x.reshape(x.shape[0], -1, self.filters[-1])
         for i in range(self.n_layers):
             x = self.pool(x, self.upsample_matrices[-i-1])
+            print('layers',self.n_layers-i)
+            print('i',i)
             x = F.relu(self.cheb_dec[i](x, self.A_edge_index[self.n_layers-i-1], self.A_norm[self.n_layers-i-1]))
             print('decoder post conv x:', x.shape)
         print('last level x:', x.shape)
-        x = self.cheb_dec[-1](x, self.A_edge_index[-1], self.A_norm[-1])
+        x = self.cheb_dec[-1](x, self.A_edge_index[0], self.A_norm[0])
         return x
 
     def reset_parameters(self):
