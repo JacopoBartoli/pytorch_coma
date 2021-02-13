dc52d149e1f1a77afa669e0d7bbf987ccd9bbc53
AntonioAcunzo <acu8397@gmail.com>
2021-02-13 10:27:05 +0100

    stampe

--- main.py
+++ main.py
@@ -164,11 +164,11 @@ def train(coma, train_loader, len_dataset, optimizer, device):
         # data: Batch(batch=[80368], edge_index=[2, 479840], x=[80368, 3], y=[80368, 3])
         # data.y.shape: torch.Size([80368, 3])
 
-        print(loss)
+        print(loss.size())
 
         # data.num_graphs : 16
 
-        total_loss += data.num_graphs * loss.item()
+        total_loss += data.num_graphs #* loss.item()
         loss.backward()
         optimizer.step()
     return total_loss / len_dataset
