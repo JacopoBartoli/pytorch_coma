441cac8a5ae0797bf7af0505a53f166572f78a87
jacopo <bartolijacopo@gmail.com>
2021-02-19 18:18:12 +0100

    Worker thread alert added.

--- main.py
+++ main.py
@@ -96,8 +96,9 @@ def main(args):
     normalize_transform = Normalize()
     dataset = ComaDataset(data_dir, dtype='train', split=args.split, split_term=args.split_term, pre_transform=normalize_transform)
     dataset_test = ComaDataset(data_dir, dtype='test', split=args.split, split_term=args.split_term, pre_transform=normalize_transform)
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)#num_workers=workers_thread
-    test_loader = DataLoader(dataset_test, batch_size=1, shuffle=False, num_workers=0)#num_workers=workers_thread
+    # Set worker thread to zero if you are debugging.
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers= workers_thread)
+    test_loader = DataLoader(dataset_test, batch_size=1, shuffle=False, num_workers= workers_thread)
 
     print('Loading model')
     start_epoch = 1
